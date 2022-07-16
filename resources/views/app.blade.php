99480eb6edcd3b55ffb46c26d2401565a54dac8e
ogzhntrsn <oguzhantursunn@gmail.com>
2022-07-16 23:57:45 +0300

    kod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -91,17 +91,6 @@
             @yield('content')
         </div>
 
-        <footer>
-            <div class="footer clearfix mb-0 text-muted">
-                <div class="float-start">
-                    <p>2021 &copy; Mazer</p>
-                </div>
-                <div class="float-end">
-                    <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                            href="http://ahmadsaugi.com">A. Saugi</a></p>
-                </div>
-            </div>
-        </footer>
     </div>
 </div>
 <script src="{{asset('dist/assets/js/app.js')}}"></script>
